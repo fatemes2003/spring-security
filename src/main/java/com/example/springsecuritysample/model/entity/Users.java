65634b029db8dc72f4532f08e4ffb0e44fe686ef
fateme <fatemeh.s2003@gmail.com>
2023-09-24 12:43:33 +0330

    jdbcAuthentication

--- src/main/java/com/example/springsecuritysample/model/entity/Users.java
+++ src/main/java/com/example/springsecuritysample/model/entity/Users.java
@@ -1,5 +1,6 @@
 package com.example.springsecuritysample.model.entity;
 
+import com.example.springsecuritysample.model.enums.UserRoles;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
@@ -9,9 +10,9 @@ import org.hibernate.annotations.UpdateTimestamp;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
-@Table(name = "users_tbl")
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Users implements Serializable {
 
@@ -37,14 +38,82 @@ public class Users implements Serializable {
     @UpdateTimestamp
     private LocalDateTime updatedAt;
 
+    @ElementCollection(targetClass = UserRoles.class)
+    @CollectionTable(name = "authorities",
+            joinColumns = @JoinColumn(name = "email", referencedColumnName = "email"))
+            @Enumerated(EnumType.STRING)
+    private List<UserRoles> userRoles;
 
-    public Users() {
-    }
-
-    public Users(String email, String password, String name, String cover) {
+    public Users(String email, String password, String name, LocalDateTime createdAt, LocalDateTime updatedAt, List<UserRoles> userRoles) {
         this.email = email;
         this.password = password;
         this.name = name;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.userRoles = userRoles;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public List<UserRoles> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(List<UserRoles> userRoles) {
+        this.userRoles = userRoles;
+    }
 }
