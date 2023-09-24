f12445153000bbb6766ad682be8c87c090d5cdc7
fateme <fatemeh.s2003@gmail.com>
2023-09-24 11:17:07 +0330

    InMemoryAuthentication

---
+++ src/main/java/com/example/springsecuritysample/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.example.springsecuritysample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/403", method = RequestMethod.GET)
+    public String accessDenied() {
+        return "403";
+    }
+}
--- src/main/java/com/example/springsecuritysample/model/entity/Users.java
+++ src/main/java/com/example/springsecuritysample/model/entity/Users.java
@@ -1,13 +1,50 @@
 package com.example.springsecuritysample.model.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "users")
-public class Users {
-    private int user_id;
-    private String username;
-    private String password;
+@Table(name = "users_tbl")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Users implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
     private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private String name;
+
+    private boolean enabled = true;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+    public Users() {
+    }
+
+    public Users(String email, String password, String name, String cover) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
 }
